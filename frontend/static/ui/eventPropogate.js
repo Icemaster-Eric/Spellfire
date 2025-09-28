02248cafca1fe35ffef10543c3bd16787403f9b6
HydrogenMacro <hydrogenmacro@gmail.com>
2025-09-28 18:03:09 -0400

    v1.0

--- frontend/static/ui/eventPropogate.js
+++ frontend/static/ui/eventPropogate.js
@@ -1,10 +1,12 @@
-/*
+
 const gameCanvas = document.getElementById("game");
 ['pointerdown', 'pointerup', "keydown", "keyup"].forEach(evtName => {
+    gameCanvas.addEventListener(evtName, console.log);
     document.addEventListener(evtName, e => {
         if (e.__a) return;
         const newEvt = new e.constructor(evtName, e);
-        newEvt.__a = true
+        newEvt.__a = true;
+        console.log("got ev")
         gameCanvas.dispatchEvent(newEvt);
     });
-});*/
+});
