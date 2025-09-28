97ba3cb87769a74087a209adae8d363ee57aac75
Eric Li <icemastereric@gmail.com>
2025-09-28 14:52:41 -0600

    small update

--- backend/internal/game/system.go
+++ backend/internal/game/system.go
@@ -3,6 +3,7 @@ package game
 import (
 	"math"
 	"math/rand"
+	"time"
 
 	"github.com/Icemaster-Eric/Spellfire/backend/internal/game/archetype"
 	"github.com/Icemaster-Eric/Spellfire/backend/internal/game/component"
@@ -56,77 +57,76 @@ func (w *World) DecreasePlayerVelocity(dt float64) { // REMOVE??
 	}
 }
 
-// ignore bullets for now
-// func (w *World) ProcessBullets(dt float64) {
-// 	playerArch := w.Archetype(entity.PlayerSignature())
-// 	playerArch.Columns.Query(func(txn *column.Txn) error {
-// 		xCol := txn.Float64("X")
-// 		yCol := txn.Float64("Y")
-// 		rotationCol := txn.Float64("ROTATION")
-// 		lastFiredCol := txn.Uint64("LAST_FIRED")
-// 		return txn.WithValue("IS_FIRING", func(v any) bool {
-// 			return v == true
-// 		}).Range(func(idx uint32) {
-// 			x, _ := xCol.Get()
-// 			y, _ := yCol.Get()
-// 			rotation, _ := rotationCol.Get()
-// 			lastFired, _ := lastFiredCol.Get()
-// 			vx := math.Cos(rotation)
-// 			vy := math.Sin(rotation)
-// 			if uint64(time.Now().UnixMilli())-lastFired > 333 { // shoot 3 times per second?
-// 				w.SpawnEntity(entity.Bullet{
-// 					X:        x + vx*0.6, // spawn bullet outside of player for now
-// 					Y:        y + vy*0.6,
-// 					VX:       vx * 5, // CHANGE THE SPEED LATER
-// 					VY:       vy * 5, // ACCORDING TO EACH GUN'S BULLET SPEED (?)
-// 					DAMAGE:   2,
-// 					LIFESPAN: 2,
-// 				})
-// 			}
-// 		})
-// 	})
+func (w *World) ProcessBullets(dt float64) {
+	playerArch := w.Archetype(entity.GunnerSignature())
+	playerArch.Columns.Query(func(txn *column.Txn) error {
+		xCol := txn.Float64("X")
+		yCol := txn.Float64("Y")
+		rotationCol := txn.Float64("ROTATION")
+		lastFiredCol := txn.Uint64("LAST_FIRED")
+		return txn.WithValue("IS_FIRING", func(v any) bool {
+			return v == true
+		}).Range(func(idx uint32) {
+			x, _ := xCol.Get()
+			y, _ := yCol.Get()
+			rotation, _ := rotationCol.Get()
+			lastFired, _ := lastFiredCol.Get()
+			vx := math.Cos(rotation)
+			vy := math.Sin(rotation)
+			if uint64(time.Now().UnixMilli())-lastFired > 333 { // shoot 3 times per second?
+				w.SpawnEntity(entity.Bullet{
+					X:        x + vx*0.6, // spawn bullet outside of player for now
+					Y:        y + vy*0.6,
+					VX:       vx * 5, // CHANGE THE SPEED LATER
+					VY:       vy * 5, // ACCORDING TO EACH GUN'S BULLET SPEED (?)
+					DAMAGE:   2,
+					LIFESPAN: 2,
+				})
+			}
+		})
+	})
 
-// 	removeBullets := []string{}
-// 	arch := w.Archetype(entity.BulletSignature())
-// 	arch.Columns.Query(func(txn *column.Txn) error {
-// 		entityIDCol := txn.Key()
-// 		xCol := txn.Float64("X")
-// 		yCol := txn.Float64("Y")
-// 		damageCol := txn.Float64("DAMAGE")
-// 		// vxCol := txn.Float64("VX")
-// 		// vyCol := txn.Float64("VY")
-// 		return txn.Range(func(idx uint32) {
-// 			entityID, _ := entityIDCol.Get()
-// 			x, _ := xCol.Get()
-// 			y, _ := yCol.Get()
-// 			damage, _ := damageCol.Get()
+	removeBullets := []string{}
+	arch := w.Archetype(entity.BulletSignature())
+	arch.Columns.Query(func(txn *column.Txn) error {
+		entityIDCol := txn.Key()
+		xCol := txn.Float64("X")
+		yCol := txn.Float64("Y")
+		damageCol := txn.Float64("DAMAGE")
+		// vxCol := txn.Float64("VX")
+		// vyCol := txn.Float64("VY")
+		return txn.Range(func(idx uint32) {
+			entityID, _ := entityIDCol.Get()
+			x, _ := xCol.Get()
+			y, _ := yCol.Get()
+			damage, _ := damageCol.Get()
 
-// 			playerArch.Columns.Query(func(playerTxn *column.Txn) error {
-// 				playerXCol := playerTxn.Float64("X")
-// 				playerYCol := playerTxn.Float64("Y")
-// 				playerRadiusCol := playerTxn.Float64("RADIUS")
-// 				playerHealthCol := playerTxn.Float64("HEALTH")
-// 				return playerTxn.Range(func(playerIdx uint32) {
-// 					playerX, _ := playerXCol.Get()
-// 					playerY, _ := playerYCol.Get()
-// 					playerRadius, _ := playerRadiusCol.Get()
+			playerArch.Columns.Query(func(playerTxn *column.Txn) error {
+				playerXCol := playerTxn.Float64("X")
+				playerYCol := playerTxn.Float64("Y")
+				playerRadiusCol := playerTxn.Float64("RADIUS")
+				playerHealthCol := playerTxn.Float64("HEALTH")
+				return playerTxn.Range(func(playerIdx uint32) {
+					playerX, _ := playerXCol.Get()
+					playerY, _ := playerYCol.Get()
+					playerRadius, _ := playerRadiusCol.Get()
 
-// 					dx := x - playerX
-// 					dy := y - playerY
+					dx := x - playerX
+					dy := y - playerY
 
-// 					if dx*dx+dy*dy <= playerRadius*playerRadius {
-// 						playerHealthCol.Merge(-damage)
-// 						removeBullets = append(removeBullets, entityID)
-// 					}
-// 				})
-// 			})
-// 		})
-// 	})
-// 	// remove bullets that have hit players
-// 	for _, entity_id := range removeBullets {
-// 		arch.Columns.DeleteKey(entity_id)
-// 	}
-// }
+					if dx*dx+dy*dy <= playerRadius*playerRadius {
+						playerHealthCol.Merge(-damage)
+						removeBullets = append(removeBullets, entityID)
+					}
+				})
+			})
+		})
+	})
+	// remove bullets that have hit players
+	for _, entity_id := range removeBullets {
+		arch.Columns.DeleteKey(entity_id)
+	}
+}
 
 func (w *World) SpawnEntities(dt float64) {
 	// loop through players
